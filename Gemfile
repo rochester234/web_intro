9514e390555f52c81340446a0f447bfac0706d8b
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-10-27 09:22:07 +0000

    start sinatra

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.2.3'
 
 gem 'rspec'
 gem 'sinatra'
+gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  'Hello Edward!'
+  'Hello Edward! test test'
+end
+
+get '/secret' do
+  'this is a page extension'
+end
+
+get '/cat' do
+  "<img src='http://bit.ly/1eze8aE'>"
 end
